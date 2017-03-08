ec54c3ac8242643acd93a0967b2cdfc256aa0f90
Shrijana2016 <Shrijana.Ghimire@mail.citytech.cuny.edu>
2017-03-08 16:05:33 -0500

    added welcome page but haven't linked it yet

---
+++ client/components/welcome.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Welcome = () => {
+
+    return (
+        <div className="row">
+            <div className="col-xs-6"></div>
+            <div className="col-xs-6">
+            <h2>Welcome Aboard!</h2>
+            <p>
+                You have succesfully signed up. We are happy to have you here. To explore more and
+                get involved with Opec, login with your useremail and password.
+             </p>
+     
+            </div>
+        </div>
+
+
+    );
+};
+export default Welcome;
