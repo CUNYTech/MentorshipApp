1110e1287a9f3a608bbb0bb3617a1dac4567c5ea
Shrijana2016 <Shrijana.Ghimire@mail.citytech.cuny.edu>
2017-03-08 14:06:58 -0500

    edited the contents of about page description

--- client/components/about.js
+++ client/components/about.js
@@ -9,16 +9,16 @@ const About = () => {
             <div className="col-xs-6">
             <h2>About us</h2>
             <p>
-                Obec takes its name from the Slovak translation from the English word community. The goal of this web application
+                Obec takes its name from the Slovak translation from the English word "community". The goal of this web application
                 is to create a community among people where they can find short-term or long-term mentors in different areas such as
                 Women in Tech, Career Advice, Life Advice, Programming, and so on.
             </p>
             <p>
                 Our logo design of a meerkat symbolizes community. Meerkats live and work together in their communities. They look out
-                for one another. Obec brings together people of different backgrounds to look out for one another. If you know something
-                and would like to share, then please contribute as a mentor. If you are looking to have a someone's point of view
-                on something you don't know or simply don't understand, then please sign up as a mentee. Here at Obec, we give you the
-                the opportunity to be both.
+                for one another. Obec brings together people from different backgrounds to look out for one another. If you know something 
+                valuable and would like to share, then please contribute as a mentor. If you are looking to have someone's point of view
+                on something you don't know or simply don't understand and hold interest on, then please sign up as a mentee. Here at Obec, we give you the
+                the opportunity to be both. Let's seek help and provide help.
             </p>
             </div>
         </div>
