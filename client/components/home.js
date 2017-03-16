ee38ea78ddba52b2ebd40728dddf5fcc28739875
aionate0812 <aionate0812@gmail.com>
2017-03-15 20:46:07 -0400

    edited home.js

--- client/components/home.js
+++ client/components/home.js
@@ -1,20 +1,55 @@
 import React from 'react';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 
-const Home = () => {
 
-    return (
-        <div className="row">
-            <div className="col-xs-6">
-                <img src="meerkat.png" />
-            </div>
-            <div className="col-xs-6">
-                <h2>Welcome</h2>
-                <p>
-                    Obec is a community of mentors where you can find a mentor or be one.
-                </p>
-            </div>
+export default class Home extends React.Component {
+
+
+    routeDashboard(){
+        var Router = require('react-router');
+        Router.browserHistory.push('/dashboard');
+
+    }
+    checkLogin() {
+        if (Meteor.userId() == undefined || Meteor.userId() == null) {
+
+return(    <div className="row">
+        <div className="col-xs-6">
+            <img src="meerkat.png"/>
         </div>
-    );
-};
-export default Home;
+        <div className="col-xs-6">
+            <h2>Welcome</h2>
+            <p>
+                Obec is a community of mentors where you can find a mentor or be one.
+            </p>
+        </div>
+    </div>
+
+)
+
+
+
+        }
+        else {
+         this.routeDashboard();
+
+        }
+
+    }
+
+
+    render() {
+return (
+    <div>
+
+        {this.checkLogin()}
+
+        </div>
+
+
+)
+
+
+    }
+}
