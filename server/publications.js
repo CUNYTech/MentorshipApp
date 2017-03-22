511ba8906cbfc1a06a51b86b6012bc8c100c47aa
Liang Chen <liang.r.chen@gmail.com>
2017-03-22 19:57:03 -0400

    add change email and password

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 /* Publish users collection to see them on CTRL + M */
 Meteor.publish('users', function() {
     return Meteor.users.find({});
 });
 
+Meteor.methods({
+  'users.updateEmail': function(email) {
+    if(email != Meteor.user().emails[0].address) {
+      Accounts.addEmail(Meteor.userId(), email);
+      Accounts.removeEmail(Meteor.userId(), Meteor.user().emails[0].address);
+    }
+  },
+
+  'users.changePassword': function(newPassword) {
+    Accounts.changePassword(Meteor.userId(), newPassword);
+  },
+});
