23bdeb4de2c6dc7a7bf7984c26bcbe42ca3e01db
Laisa <laisa.barros26@gmail.com>
2017-03-31 23:28:06 -0400

    added Maz's changes from urlProfile and fixed merging conflict; dashboard, header, profile, main.js

--- client/components/profile.js
+++ client/components/profile.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import { Meteor }           from 'meteor/meteor';
+import { Meteor }           from 'meteor/meteor'
 import { createContainer }  from 'meteor/react-meteor-data';
+import { Accounts } from  'meteor/accounts-base'
 
 class Profile extends Component {
     constructor(props) {
         super(props);
-        this.state = { isEditProfile: false, isEditAccount: false };
+
+
+
+        this.state = {  isEditProfile: false, isEditAccount: false };
+
+
+
     }
 
-    getName() {
-        return this.props.user.profile.firstName + ' ' + this.props.user.profile.lastName;
-    }
+    validProfile(){
+        user=  Meteor.users.findOne({ username : this.props.params.username})
 
+        if(user && user!='undefined' && user != 'null') return true;
+
+        return false;
+
+
+    }
+    ownProfile() {
+        return this.props.user.username ==Meteor.users.findOne({ username : this.props.params.username}).username
+
+
+    }
     getAvatar() {
-        if(this.props.user.profile.avatar != '')
-            return this.props.user.profile.avatar;
+        if(this.props.params.username==='undefined' || this.props.params.username===null) var user = this.props.user;
+        else var user = Meteor.users.findOne({ username : this.props.params.username});
+
+        if (user.profile.avatar != '')
+            return user.profile.avatar;
         else
-            return  "default-user.png";
+            return "default-user.png";
     }
+
 
     getProfile() {
+        var user = Meteor.users.findOne({ username : this.props.params.username});
+
         return (
             <div id="put-bottom">
                 <h2>{this.props.user.profile.firstName}</h2>
@@ -60,60 +83,48 @@ class Profile extends Component {
     saveProfile() {
         const name = this.refs.firstName.value;
         const blurb = this.refs.blurb.value;
+        const tags = this.refs.tags.value;
         Meteor.users.update(Meteor.userId(), {$set: {
             "profile.firstName": name,
             "profile.blurb": blurb,
+            "profile.tags": tags,
         }});
         this.setState({isEditProfile: false});
     }
 
     editAccount() {
-      this.setState({isEditAccount: true});
+        this.setState({isEditAccount: true});
     }
 
     cancelAccount() {
-      this.setState({isEditAccount: false});
+        this.setState({isEditAccount: false});
     }
 
     saveAccount() {
-      const email = this.refs.email.value;
-      const newPassword = this.refs.newPassword.value;
-      const conPassword = this.refs.conPassword.value;
-      if(newPassword != conPassword) {
-        console.log("passwords don't match");
-      }
-      else if(newPassword == '') {
-        Meteor.call('users.updateEmail', email);
-        this.setState({isEditAccount: false});
-      }
-      else {
-        Meteor.call('users.updateEmail', email);
-        Meteor.call('users.changePassword', newPassword);
-        this.setState({isEditAccount: false});
-      }
+        const email = this.refs.email.value;
+        const newPassword = this.refs.newPassword.value;
+        const conPassword = this.refs.conPassword.value;
+        if(newPassword != conPassword) {
+            console.log("passwords don't match");
+        }
+        else if(newPassword == '') {
+            Meteor.call('users.updateEmail', email);
+            this.setState({isEditAccount: false});
+        }
+        else {
+            Meteor.call('users.updateEmail', email);
+            Meteor.call('users.changePassword', newPassword);
+            this.setState({isEditAccount: false});
+        }
     } //end saveAccount()
 
-    addMentorTags(event) {
-      event.preventDefault();
-      tags = this.refs.mentortags.value;
-      if (tags != "") {
-          Meteor.call('users.addMentorTags', tags);
-      }
-    }
-
-    addMenteeTags(event) {
-      event.preventDefault();
-      tags = this.refs.menteetags.value;
-      if (tags != "") {
-          Meteor.call('users.addMenteeTags', tags);
-      }
-    }
-
     render() {
-        if(!this.props.user) {
+
+
+        if(!this.props.user ) {
             return <div>Loading...</div>;
         }
-        else if(this.state.isEditProfile) {
+        else if(this.state.isEditProfile && this.ownProfile() ) {
             return (
                 <div className="row">
                     <div className="col-md-4 col-md-offset-4">
@@ -126,32 +137,21 @@ class Profile extends Component {
                             <p>
                                 <label>Blurb</label>
                                 <textarea ref="blurb" className="form-control" type="text"
-                                     defaultValue={this.props.user.profile.blurb} placeholder="Describe yourself here... "
+                                          defaultValue={this.props.user.profile.blurb} placeholder="Describe yourself here... "
                                           id="profile_blurb" rows="4" cols="50" maxLength="500">
 
                             </textarea>
 
                             </p>
                             <p>
-                                <label>Mentor Tags</label>
-                                <input ref="mentortags" className="form-control" type="text"
-                                     placeholder="Enter tags to include yourself in mentor search result"
-                                     id="mentor_tags">
-                                </input>
-                                <a onClick={this.addMentorTags.bind(this)}>
-                                    <img className="plusIcon" src="plus-icon.png"/>
-                                </a>
+                                <label>Tags</label>
+                                <textarea ref="tags" className="form-control" type="text"
+                                          defaultValue={this.props.user.profile.tags} placeholder="Type a role that best describes you-- Mentor/Mentee..."
+                                          id="profile_tags" rows="4" cols="5" maxLength="500">
+
+                            </textarea>
                             </p>
-                            <p>
-                                <label>Mentee Tags</label>
-                                <input ref="menteetags" className="form-control" type="text"
-                                     placeholder="Enter tags to include yourself in mentee search result"
-                                     id="mentee_tags">
-                                </input>
-                                <a onClick={this.addMenteeTags.bind(this)}>
-                                    <img className="plusIcon" src="plus-icon.png"/>
-                                </a>
-                            </p>
+
                         </form>
 
                         <div className="buttons">
@@ -168,36 +168,36 @@ class Profile extends Component {
                 </div>
             );
         }
-        else if(this.state.isEditAccount) {
-          return (
-              <div className="row">
-                  <div className="col-md-4 col-md-offset-4">
-                      <form className="form-horizontal">
-                          <p>
-                              <label>Email</label>
-                              <input ref="email" className="form-control" type="text"
-                                     defaultValue={this.props.user.emails[0].address} />
-                          </p>
-                          <p>
-                              <label>New Password</label>
-                              <input ref="newPassword" className="form-control" type="password" />
-                              <label>Confirm Password</label>
-                              <input ref="conPassword" className="form-control" type="password" />
-                          </p>
-                      </form>
-                      <div className="buttons">
-                          <button className="btn btn-danger" onClick={() => this.cancelAccount()}>
-                              Cancel
-                          </button>
-                          <div className="floatRight">
-                              <button className="btn btn-success" onClick={() => this.saveAccount()}>
-                                  Save
-                              </button>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          );
+        else if(this.state.isEditAccount && this.ownProfile() ) {
+            return (
+                <div className="row">
+                    <div className="col-md-4 col-md-offset-4">
+                        <form className="form-horizontal">
+                            <p>
+                                <label>Email</label>
+                                <input ref="email" className="form-control" type="text"
+                                       defaultValue={this.props.user.emails[0].address} />
+                            </p>
+                            <p>
+                                <label>New Password</label>
+                                <input ref="newPassword" className="form-control" type="password" />
+                                <label>Confirm Password</label>
+                                <input ref="conPassword" className="form-control" type="password" />
+                            </p>
+                        </form>
+                        <div className="buttons">
+                            <button className="btn btn-danger" onClick={() => this.cancelAccount()}>
+                                Cancel
+                            </button>
+                            <div className="floatRight">
+                                <button className="btn btn-success" onClick={() => this.saveAccount()}>
+                                    Save
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
         }
         else {
             return (
@@ -216,7 +216,14 @@ class Profile extends Component {
 }; // end class Profile
 
 
+//<input ref="blurb" className="form-control" type="text"
+//     defaultValue={this.props.user.profile.blurb} />
+
+
 export default createContainer(() => {
     //return an object, Whatever we return will be send to userList as props
+
+
+
     return { user: Meteor.user()};
 }, Profile);
