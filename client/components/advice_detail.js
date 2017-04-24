db028b4378ee1ad8f403526e287869e4e1d93d3c
Laisa <laisa.barros26@gmail.com>
2017-04-24 16:36:21 -0400

    added rendering of emojis to advices

--- client/components/advice_detail.js
+++ client/components/advice_detail.js
@@ -5,10 +5,15 @@ import { Advices }          from '../../imports/collections/advices';
 
 /* this.props.advice refers to the advice object in Advices collection */
 class AdviceDetail extends Component {
+    onRemoveAdvice(advice) {
+        Meteor.call("advices.remove", advice);
+    }
 
-  onRemoveAdvice(advice) {
-    Meteor.call("advices.remove", advice);
-  }
+    renderEmoji(text) {
+        return (
+            <span dangerouslySetInnerHTML={{__html: Emojis.parse(text)}} />
+        )
+    }
 
   render() {
     if(this.props.loading) {
@@ -19,7 +24,7 @@ class AdviceDetail extends Component {
         <div>
           <br/>
               <div id="advices">
-                  {this.props.advice.message}
+                  {this.renderEmoji(this.props.advice.message)}
               <p>{Meteor.userId() === this.props.advice.ownerId &&
               <span><a id="delete" onClick={() => this.onRemoveAdvice(this.props.advice)}> Delete
               </a></span>
@@ -32,7 +37,9 @@ class AdviceDetail extends Component {
 };
 
 export default createContainer((props) => {
-  const loading = !Meteor.subscribe('users').ready();
+    Meteor.subscribe('emojis');
 
-  return { loading: loading };
+    const loading = !Meteor.subscribe('users').ready();
+
+    return { loading: loading, emojis: Emojis.find({}) };
 }, AdviceDetail);
