fad50141be96720ece5755a3e8b20955a5c5ad92
Laisa <laisa.barros26@gmail.com>
2017-04-27 14:34:15 -0400

    added filter dropdown back to main-search

--- client/components/main-search.js
+++ client/components/main-search.js
@@ -7,8 +7,44 @@ import UserResults          from './user_results';
 class MainSearch extends Component {
 
     constructor(props){
-        super(props)
-    };
+        super(props);
+        this.state = {users:[], option: 'user'};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({option: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const search = this.refs.searchBox.value;
+        const option = this.state.option;
+
+        if(option === 'user') {
+            Meteor.call('searchUsers', search, (error, user) => {
+                if(user) {
+                    this.setState({users: user});
+                }
+            });
+        } //end if
+        else if(option === 'mentor') {
+            Meteor.call('searchMentors', search, (error, users) => {
+                if(users) {
+                    this.setState({users: users});
+                }
+            });
+        } //end else if
+        else {
+            Meteor.call('searchMentees', search, (error, users) => {
+                if(users) {
+                    this.setState({users: users});
+                }
+            });
+        }
+        //end else
+    } //end handleSubmit
 
 
     render(){
@@ -20,16 +56,21 @@ class MainSearch extends Component {
                         <input type="search" className="form-control" placeholder="Search"/>
                         <img className="dropdown-toggle" id="search-icon" src="/search-icon.png"/>
                     </div>
-                    <p className="bold">Contacts</p>
-                    <ul id="list-filters">
-                        <p><option className="">Mentors</option></p>
-                        <p><option className="">Mentees</option></p>
-                    </ul>
+                    {/*<p className="bold">Contacts</p>*/}
+                    {/*<ul id="list-filters">*/}
+                        {/*<p><option className="">Mentors</option></p>*/}
+                        {/*<p><option className="">Mentees</option></p>*/}
+                    {/*</ul>*/}
                     <p className="bold">Search By</p>
-                    <ul id="list-filters">
-                        <p><option className="">Users</option></p>
-                        <p><option className="">Tags</option></p>
-                    </ul>
+                    <div>
+                    <div>
+                        <select className="form-control" value={this.state.option} onChange={this.handleChange}>
+                            <option value="user" defaultValue>User</option>
+                            <option value="mentor">Mentor</option>
+                            <option value="mentee">Mentee</option>
+                    </select>
+                    </div>
+                    </div>
                 </div>
                 <div className="col-xs-6">
                     <UserResults tags = {this.props.params.tag} role = {this.props.params.role} />
